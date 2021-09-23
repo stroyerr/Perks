3465fb1d622548a86c41cf35eed3b01d1d322b3e
Angus <angusoneill972@gmail.com>
2021-09-23 13:12:04 +1000

    allyoucaneat perk

--- src/main/java/org/stroyer/perks/Perks/Perk.java
+++ src/main/java/org/stroyer/perks/Perks/Perk.java
@@ -32,17 +32,20 @@ public class Perk implements Serializable {
     private String name;
     private int cost;
     private String[] description;
-    private int id;
+    protected final int id;
     private String[] usage;
 
     public static Perk Solo = new Perk("Solo", 1, new String[]{"Expirience Solo Play!", "Toggle other players visibility!"}, new String[]{"Use /solo to toggle this perk"});
-    public static Perk PunchGun = new Perk("Punch Gun", 3, new String[]{"Knock other players back!", "Use your punch gun to gain distance!"}, new String[]{"Use /punch to get your", "punch gun!"});
-    public static Perk TPBow = new Perk("Teleport Bow", 2, new String[]{"Teleport by shotting","a bow!"}, new String[]{ "Use /tpbow or /bow","to get your bow!"});
+// Coming soon!    public static Perk Launch = new Perk("Launch", 1, new String[]{"Launch into the sky!","Get a boost to start","your elytra flight!"}, new String[]{"Hold crouch then jump","to launch into the sky!"});
     public static Perk SuperSpy = new Perk("Super Spy", 1, new String[]{"Spy on people","from any distance!"}, new String[]{"Use /superspy to get","your Super Spy tool!"});
+    public static Perk TPBow = new Perk("Teleport Bow", 2, new String[]{"Teleport by shotting","a bow!"}, new String[]{ "Use /tpbow or /bow","to get your bow!"});
+    public static Perk NightVision = new Perk("Night Vision", 2, new String[]{"Get permanent night vision!"}, new String[]{"Use /nightvision or /nv to","toggle night vision!"});
+    public static Perk FireProof = new Perk("Fire Proof", 2, new String[]{"Never burn again!","Fire can't touch you now!"}, new String[]{"When you step on fire, ","lava or otherwise","catch on fire, you","will take no damage!"});
+    public static Perk PunchGun = new Perk("Punch Gun", 3, new String[]{"Knock other players back!", "Use your punch gun to gain distance!"}, new String[]{"Use /punch to get your", "punch gun!"});
     public static Perk Parachute = new Perk("Parachute", 3, new String[]{"Never take fall damage again!", "Save yourself from heights!"}, new String[]{"When falling from more","than five blocks, your","parachute will automatically","deploy, saving you","from fall damage!"});
+    public static Perk AllYouCanEat = new Perk("All You Can Eat", 3, new String[]{"Never go hungry","again!"},new String[]{"You'll never lose hunger again!"});
 
-
-    private static List<Perk> allPerks = Arrays.asList(Solo, TPBow, PunchGun, SuperSpy, Parachute);
+    private static List<Perk> allPerks = Arrays.asList(Solo, TPBow, PunchGun, FireProof, SuperSpy, NightVision, Parachute, AllYouCanEat);
 
     public Perk(String name, int cost, String[] description, String[] usage){
         this.name = name;
